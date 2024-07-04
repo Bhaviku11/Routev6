3e6c500be902b166c64b363339cc67f7ae80f46b
Bhavik <bhavnipatel60@gmail.com>
2024-07-04 09:59:45 -0400

    Added new pages

--- src/AllRoute.tsx
+++ src/AllRoute.tsx
@@ -1,25 +1,31 @@
-import {
-  BreadcrumbsItem,
-  BreadcrumbsProvider,
-} from "react-breadcrumbs-dynamic";
+import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
 import { Route, Routes } from "react-router-dom";
+import DeliveryManagement from "./pages/DeliveryManagement";
 import FleedManagement from "./pages/FleetManagement";
 import Layout from "./pages/Layout";
 import LoadingSheet from "./pages/LoadingSheet";
-import Dashboard from './pages/dashboard';
+import OrderTracking from "./pages/OrderTracking";
+import RouteCreation from "./pages/RouteCreation";
+import Dashboard from "./pages/dashboard";
 
 function AllRoute() {
-  return (
-    <BreadcrumbsProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/loading-sheets" element={<LoadingSheet />} />
-          <Route path="/fleet-management" element={<FleedManagement />} />
-        </Route>
-      </Routes>
-    </BreadcrumbsProvider>
-  );
+    return (
+        <BreadcrumbsProvider>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/loading-sheets" element={<LoadingSheet />} />
+                    <Route
+                        path="/fleet-management"
+                        element={<FleedManagement />}
+                    />
+                    <Route path="/delivery" element={<DeliveryManagement />} />
+                    <Route path="/order-tracking" element={<OrderTracking />} />
+                    <Route path="/route-creation" element={<RouteCreation />} />
+                </Route>
+            </Routes>
+        </BreadcrumbsProvider>
+    );
 }
 
 export default AllRoute;
